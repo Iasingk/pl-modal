b8c1b6f59dc590b70457e0ac74e8125196e15190
sexar <iconfidence@gmail.com>
2017-10-01 01:11:27 -0500

    Update package.json file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,86 +1,83 @@
 /**
  * Created by cesarmejia on 22/08/2017.
  */
-(function() {
-	'use strict';
-
-    let gulp       = require('gulp'),
-        concat     = require('gulp-concat'),
-        typescript = require('gulp-typescript'),
-        uglify     = require('gulp-uglify'),
-        livereload = require('gulp-livereload');
+const gulp       = require('gulp');
+const concat     = require('gulp-concat');
+const typescript = require('gulp-typescript');
+const uglify     = require('gulp-uglify');
+const plumber    = require('gulp-plumber');
+const livereload = require('gulp-livereload');
 
 
-    let srcPath = {
-        css : 'styles/',
-        ts  : 'scripts/ts/',
-        root: ''
+let srcPath = {
+    css : 'styles/',
+    ts  : 'scripts/ts/',
+    root: ''
+};
+
+let destPath = {
+    js: 'scripts/js/'
+};
+
+// ---------------------------------------------------------------------
+// | Maintains updated src changes in the browser.                     |
+// ---------------------------------------------------------------------
+
+/**
+ * Reload on change.
+ */
+gulp.task('reload', ['ts'], () => {
+    gulp.src(srcPath.root)
+        .pipe(livereload());
+});
+
+/**
+ * Monitors changes in projects files and apply changes instantly.
+ * Use with livereload chrome extension.
+ * Reference: https://github.com/vohof/gulp-livereload
+ */
+gulp.task('watch', () => {
+    // Files to be watched.
+    let files = [
+        srcPath.ts   + '**/*.ts',
+        srcPath.css  + '**/*.css',
+        srcPath.root + '*.html'
+    ];
+
+    livereload.listen();
+
+    gulp.watch(files, ['ts', 'reload']);
+});
+
+
+// ---------------------------------------------------------------------
+// | Build production project.                                         |
+// ---------------------------------------------------------------------
+
+/**
+ * Concatenate and compile typescript files.
+ * Reference: https://www.npmjs.com/package/gulp-typescript/
+ */
+gulp.task('ts', () => {
+    let opts = {
+        target        : 'ES5',
+        removeComments: false,
+        noImplicitAny : false
     };
 
-    let destPath = {
-        js: 'scripts/js/'
-    };
+    // Source files.
+    let srcFiles = [
+        '!' + srcPath.ts + '**/backup.ts',
+        srcPath.ts + '**/*.ts'
+    ];
 
-    // ---------------------------------------------------------------------
-    // | Maintains updated src changes in the browser.                     |
-    // ---------------------------------------------------------------------
+    // Output file.
+    let outputFile = 'pl-modal.min.ts';
 
-    /**
-     * Reload on change.
-     */
-    gulp.task('reload', () => {
-        gulp.src(srcPath.root)
-            .pipe(livereload());
-    });
-
-    /**
-     * Monitors changes in projects files and apply changes instantly.
-     * Use with livereload chrome extension.
-     * Reference: https://github.com/vohof/gulp-livereload
-     */
-    gulp.task('watch', () => {
-        // Files to be watched.
-        let files = [
-            srcPath.ts   + '**/*.ts',
-            srcPath.css  + '**/*.css',
-            srcPath.root + '*.html'
-        ];
-
-        livereload.listen();
-
-        gulp.watch(files, ['ts', 'reload']);
-    });
-
-
-    // ---------------------------------------------------------------------
-    // | Build production project.                                         |
-    // ---------------------------------------------------------------------
-
-    /**
-     * Concatenate and compile typescript files.
-     * Reference: https://www.npmjs.com/package/gulp-typescript/
-     */
-    gulp.task('ts', () => {
-        let opts = {
-            target        : 'ES5',
-            removeComments: false,
-            noImplicitAny : false
-        };
-
-        // Source files.
-        let srcFiles = [
-            '!' + srcPath.ts + '**/backup.ts',
-            srcPath.ts + '**/*.ts'
-        ];
-
-        // Output file.
-        let outputFile = 'pl-modal.min.ts';
-
-        return gulp.src(srcFiles)
-            .pipe(concat(outputFile))
-            .pipe(typescript(opts))
-            .pipe(uglify())
-            .pipe(gulp.dest(destPath.js));
-    });
-
-})();
+    return gulp.src(srcFiles)
+        .pipe(plumber())
+        .pipe(concat(outputFile))
+        .pipe(typescript(opts))
+        .pipe(uglify())
+        .pipe(gulp.dest(destPath.js));
+});
